46cb92797845692a0913764341e63f9023640097
maddieemert <maddieemert@gmail.com>
2026-01-17 01:39:18 +0000

    Add dataset, Python script, and update requirements

---
+++ day2code.py
@@ -0,0 +1,31 @@
+#%%
+
+# Terminal path: 
+# @maddieemert ➜ /workspaces/daytwostuff (main)
+
+# Should you include the environment in your repo or not?
+# No, you shouldn't include the environment in your repo.
+
+# New terminal path:
+# (.venv) @maddieemert ➜ /workspaces/daytwostuff (main)
+# It is different because it has (.venv) in front, but otherwise the rest of the path is the same.
+
+#%%
+import pandas as pd
+df = pd.read_csv("ai_job_trends_dataset.csv")
+df.head()
+# %%
+
+# What do you notice about the extension menu?
+# I notice that the extension menu is very organized, allowing you to search and filter results for any extensions you may need (in this case, Data Wrangler). It was very easy to download directly from the menu, and clicking on it gave a more detailed analysis of the extension.
+
+# What are three useful elements of Data Wrangler?
+# Three useful elements of Data Wrangler are that it allows you to easily analyze your data, displays column statistics and visualizations, and generates Pandas code during data transformation.
+
+# Plotly version:
+# 6.5.1
+
+# Why do we use a requirements.txt file?
+# A requirements.txt file allows for consistent and reproducible Python environments by compiling all required packages and their versions together into one list.
+
+# %%
